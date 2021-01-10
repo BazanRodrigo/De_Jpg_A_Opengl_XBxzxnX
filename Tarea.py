a2f5ca13d9cc32886487af26e48db3c9a67687c1
Rodrigo Bazán Zurita <bazanzuritarodrigo@gmail.com>
2021-01-09 21:18:51 -0600

    Ya tiene botones para cambiar el modo de lineas

--- Tarea.py
+++ Tarea.py
@@ -1,16 +1,38 @@
 import pygame
 from pygame.locals import *
 
+def ubication(position):
+    if(position[0]>100 and position[0]<250):    #Lines
+        return("glBegin(GL_LINES);")
+    if(position[0]>300 and position[0]<450):    #Loop
+        return("glBegin(GL_LINES_LOOP);")
+    if(position[0]>500 and position[0]<650):    #STRIP
+        return("glBegin(GL_LINES_POINT);")
+    if(position[0]>700 and position[0]<850):    #POINT
+        return("glBegin(GL_LINES_STRIP);")
+    if(position[0]>900 and position[0]<1050):    #POINT
+        return("---------------------------cambio---------------------------------")
+    return ''
+
 def main():
     print('Ingresa el nombre de tu imagen que esta en la carpteta Raiz con extension')
     #nameImage = input('Ejemplo: cabra.jpg\n')
     cabra = pygame.image.load('cabra.jpg')
-    wp = pygame.image.load('WallpaperEtiqueta.jpg')
+    lines = pygame.image.load('lines.jpg')
+    loop = pygame.image.load('loop.jpg')
+    strip = pygame.image.load('strip.jpg')
+    point = pygame.image.load('point.jpg')
+    change = pygame.image.load('cambio.jpg')
     pygame.init()
     pygame.display.init()
     window = pygame.display.set_mode()
     window.fill((255,255,255))
     window.blit(cabra, (100,100))
+    window.blit(lines, (100,800))
+    window.blit(loop, (300,800))
+    window.blit(point, (500,800))
+    window.blit(strip, (700,800))
+    window.blit(change, (900,800))
     height = cabra.get_height()
     width = cabra.get_width()
     pygame.display.flip()
@@ -34,14 +56,21 @@ def main():
                 dy = down[1]
                 x = (pygame.mouse.get_pos()[0]-100)
                 y = height-(pygame.mouse.get_pos()[1]-100)
-                print('glVertex2i(',x,',',y,');')
+                if(dy>800 and dy<900):
+                    if (ubication(pygame.mouse.get_pos()))!='':
+                        print(ubication(pygame.mouse.get_pos()))
+                    mood = False
+                else:
+                    print('glVertex2i(',x,',',y,');')
+                    mood = True
             if event.type == pygame.MOUSEBUTTONUP:
                 up = pygame.mouse.get_pos()
                 ux = up[0]
                 uy = up[1]
                 ux = ux + 600
                 uy = uy
-                pygame.draw.line(window, RED, (dx, dy), (ux, uy),10)
+                if mood:
+                    pygame.draw.line(window, RED, (dx, dy), (ux, uy),10)
             pygame.display.flip()
     print('\n\n\n\nPorfa sigueme en mis redes sociales pq tengo poquitos seguidores')
     print('twitter: @BazanZuritaRodr')
